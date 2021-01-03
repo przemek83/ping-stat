d71faa392505bb971ab2a1f132f09f63c4c6933e
przemek83 <4788832+przemek83@users.noreply.github.com>
2021-01-03 15:58:35 +0100

    Added missing constness.

--- src/PingerLinux.cpp
+++ src/PingerLinux.cpp
@@ -11,15 +11,16 @@ QString PingerLinux::getPingCommand() const
 
 PingData PingerLinux::extractPingData(QString pingOutput) const
 {
-    QStringList outputLines{pingOutput.split('\n', QString::SkipEmptyParts)};
-    int packetsLineIndex{
+    const QStringList outputLines{
+        pingOutput.split('\n', QString::SkipEmptyParts)};
+    const int packetsLineIndex{
         outputLines.indexOf(QRegExp(".+packets transmitted.*"))};
     if (packetsLineIndex == -1)
         return {};
 
-    auto [packetsSent, packetsReceived] =
+    const auto [packetsSent, packetsReceived] =
         getPacketsInfo(outputLines[packetsLineIndex]);
-    int timesIndex{outputLines.lastIndexOf(QRegExp(".+=.+"))};
+    const int timesIndex{outputLines.lastIndexOf(QRegExp(".+=.+"))};
     if (timesIndex == -1)
         return {QDateTime::currentDateTime(),
                 packetsSent,
@@ -28,7 +29,7 @@ PingData PingerLinux::extractPingData(QString pingOutput) const
                 0,
                 0};
 
-    auto [min, avg, max] = getTimesInfo(outputLines[timesIndex]);
+    const auto [min, avg, max] = getTimesInfo(outputLines[timesIndex]);
     return {QDateTime::currentDateTime(),
             packetsSent,
             packetsSent - packetsReceived,
@@ -41,22 +42,27 @@ std::pair<int, int> PingerLinux::getPacketsInfo(
     const QString& packetsLine) const
 {
     int endIndex{packetsLine.indexOf(" ")};
-    int packetsSent{packetsLine.midRef(0, endIndex).toInt()};
-    int startIndex{packetsLine.indexOf(", ")};
-    endIndex = packetsLine.indexOf(" received", startIndex + 2);
-    int packetsReceived{
-        packetsLine.midRef(startIndex + 2, endIndex - startIndex - 2).toInt()};
+    const int packetsSent{packetsLine.midRef(0, endIndex).toInt()};
+    const QString startSubString{", "};
+    const int startIndex{packetsLine.indexOf(startSubString)};
+    endIndex =
+        packetsLine.indexOf(" received", startIndex + startSubString.size());
+    const int packetsReceived{
+        packetsLine
+            .midRef(startIndex + startSubString.size(),
+                    endIndex - startIndex - startSubString.size())
+            .toInt()};
     return {packetsSent, packetsReceived};
 }
 
 std::tuple<int, int, int> PingerLinux::getTimesInfo(
     const QString& timesLine) const
 {
-    int startIndex{timesLine.indexOf(" = ")};
-    int endIndex{timesLine.lastIndexOf(" ms")};
-    QString timesString{
+    const int startIndex{timesLine.indexOf(" = ")};
+    const int endIndex{timesLine.lastIndexOf(" ms")};
+    const QString timesString{
         timesLine.mid(startIndex + 3, endIndex - startIndex - 3)};
-    QStringList times{timesString.split('/')};
+    const QStringList times{timesString.split('/')};
     if (times.size() < 3)
         return {0, 0, 0};
 
