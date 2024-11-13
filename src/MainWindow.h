8b4c5fa683797bdfda387526dda7037faed716ee
przemek83 <4788832+przemek83@users.noreply.github.com>
2024-11-13 14:13:54 +0100

    Add missing override.

--- src/MainWindow.h
+++ src/MainWindow.h
@@ -20,7 +20,7 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget* parent = nullptr);
 
-    ~MainWindow();
+    ~MainWindow() override;
 
 private:
     void setupAdressValidator();
